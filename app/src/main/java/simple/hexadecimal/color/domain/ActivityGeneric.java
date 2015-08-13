eb9883648fb85fe2e9f32030c7fcad6918c5489a
pedrofsn <pedrokra@gmail.com>
2015-08-13 09:15:02 -0300

    Atualizando bibliotecas

--- app/src/main/java/simple/hexadecimal/color/domain/ActivityGeneric.java
+++ app/src/main/java/simple/hexadecimal/color/domain/ActivityGeneric.java
@@ -1,9 +1,10 @@
 package simple.hexadecimal.color.domain;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by pedro.sousa on 15/07/15.
  */
-public class ActivityGeneric extends ActionBarActivity {
+public class ActivityGeneric extends AppCompatActivity {
 }
